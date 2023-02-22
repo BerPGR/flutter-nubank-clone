ba946bf4152eb6b9dfc644977b157237f6a4d133
Bernardo Matuchewski <bernardoPGR01@gmail.com>
2023-02-22 19:20:32 -0300

    Added top bar in a different widget

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import '../colors/palette.dart';
+import '../widgets/top_bar_widget.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -23,86 +24,7 @@ class _HomePageState extends State<HomePage> {
           child: SingleChildScrollView(
             child: Column(
               children: [
-                Container(
-                  height: 130,
-                  decoration: BoxDecoration(
-                    color: Palette.nuPurple,
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.all(16),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Badge(
-                              padding: EdgeInsets.all(20),
-                              backgroundColor: Colors.white,
-                              child: Container(
-                                width: 45,
-                                height: 45,
-                                decoration: BoxDecoration(
-                                    color: Color(0xFF9436E2),
-                                    borderRadius: BorderRadius.circular(22)),
-                                child: Icon(
-                                  Icons.image_outlined,
-                                  color: Colors.white,
-                                ),
-                              ),
-                            ),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceAround,
-                              children: [
-                                GestureDetector(
-                                    onTap: () {
-                                      setState(() {
-                                        canSee = !canSee;
-                                      });
-                                    },
-                                    child: Image.asset(
-                                      canSee
-                                          ? 'assets/images/eye-opened.png'
-                                          : 'assets/images/hidden.png',
-                                      height: 30,
-                                    )),
-                                Padding(
-                                  padding: const EdgeInsets.symmetric(
-                                      horizontal: 15),
-                                  child: IconButton(
-                                      onPressed: () {},
-                                      icon: Icon(
-                                        Icons.help_outline_rounded,
-                                        color: Colors.white,
-                                        size: 25,
-                                      )),
-                                ),
-                                GestureDetector(
-                                  onTap: () {},
-                                  child: Image.asset(
-                                    'assets/images/add-user.png',
-                                    height: 20,
-                                  ),
-                                ),
-                              ],
-                            )
-                          ],
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.only(left: 5, bottom: 5),
-                          child: Text(
-                            'Olá, Be',
-                            style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 20,
-                                fontWeight: FontWeight.bold),
-                          ),
-                        )
-                      ],
-                    ),
-                  ),
-                )
+                TopSectionWidget(eyeOpened: canSee),
               ],
             ),
           ),
