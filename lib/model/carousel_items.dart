bdc77c2c897f7b9ae28b6d7da7393d8988c46aac
Bernardo Matuchewski <bernardoPGR01@gmail.com>
2023-02-23 15:59:06 -0300

    Created items model for main content carousel

---
+++ lib/model/carousel_items.dart
@@ -0,0 +1,22 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+class Item {
+  final String iconPath;
+  final String type;
+  Item({
+    required this.iconPath,
+    required this.type,
+  });
+}
+
+List<Item> items = [
+  Item(iconPath: 'assets/images/pix.png', type: 'Area Pix'),
+  Item(iconPath: 'assets/images/codigo-de-barras.png', type: 'Pagar'),
+  Item(iconPath: 'assets/images/transferir-dinheiro.png', type: 'Transferir'),
+  Item(iconPath: 'assets/images/iphone.png', type: 'Recarga'),
+  Item(iconPath: 'assets/images/deposito.png', type: 'Depositar'),
+  Item(iconPath: 'assets/images/cobrar.png', type: 'Cobrar'),
+  Item(iconPath: 'assets/images/internacional.png', type: 'Internacional'),
+  Item(iconPath: 'assets/images/coracao.png', type: 'Doação'),
+  Item(iconPath: 'assets/images/investir.png', type: 'Investir'),
+  Item(iconPath: 'assets/images/caixinha.png', type: 'Caixinha'),
+];
