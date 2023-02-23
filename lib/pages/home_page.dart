461674f2fd95e62baf5ef89ec0e22f51bbdd4989
Bernardo Matuchewski <bernardoPGR01@gmail.com>
2023-02-23 14:43:35 -0300

    Created first section with account and balance

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -26,13 +26,108 @@ class _HomePageState extends State<HomePage> {
             child: Column(
               children: [
                 //Top bar widget
-                TopSectionWidget(eyeOpened: canSee),
-                MainContentWidget(),
+                topSectionWidget(),
+                MainContentWidget(canSee: canSee),
               ],
             ),
           ),
         ),
       ]),
+    );
+  }
+
+  Widget topSectionWidget() {
+    return Container(
+      height: 130,
+      decoration: BoxDecoration(
+        color: Palette.nuPurple,
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                /*
+                  Badge that goes to the top-right
+                  part of the container
+                 */
+                Badge(
+                  padding: EdgeInsets.all(20),
+                  backgroundColor: Colors.white,
+                  child: Container(
+                    width: 45,
+                    height: 45,
+                    decoration: BoxDecoration(
+                        color: Color(0xFF9436E2),
+                        borderRadius: BorderRadius.circular(22)),
+                    /*
+                          Icon inside container to add a certain image idk
+                         */
+                    child: Icon(
+                      Icons.image_outlined,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    /*
+                      Gesture detector to change info behaviours
+                     */
+                    GestureDetector(
+                        onTap: () {
+                          setState(() {
+                            canSee = !canSee;
+                          });
+                        },
+                        child: Image.asset(
+                          canSee
+                              ? 'assets/images/eye-opened.png'
+                              : 'assets/images/hidden.png',
+                          height: 30,
+                        )),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 15),
+                      child: IconButton(
+                          onPressed: () {},
+                          icon: Icon(
+                            Icons.help_outline_rounded,
+                            color: Colors.white,
+                            size: 25,
+                          )),
+                    ),
+                    GestureDetector(
+                      onTap: () {},
+                      child: Image.asset(
+                        'assets/images/add-user.png',
+                        height: 20,
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
+            /*
+            Padding to fix text position
+             */
+            Padding(
+              padding: const EdgeInsets.only(left: 5, bottom: 5),
+              child: Text(
+                'Olá, Be',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold),
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/widgets/main_content_widget.dart
+++ lib/widgets/main_content_widget.dart
@@ -1,7 +1,12 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
 
 class MainContentWidget extends StatefulWidget {
-  const MainContentWidget({super.key});
+  final bool canSee;
+  const MainContentWidget({
+    Key? key,
+    required this.canSee,
+  }) : super(key: key);
 
   @override
   State<MainContentWidget> createState() => _MainContentWidgetState();
@@ -10,15 +15,42 @@ class MainContentWidget extends StatefulWidget {
 class _MainContentWidgetState extends State<MainContentWidget> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(20),
+    bool eyeOpen = widget.canSee;
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 30),
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         /*
           First section contains a Text and balance
         */
         children: [
-          // Text with account
-          Text("Conta"),
+          // Row with text and arrow to check account
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                children: [
+                  Text(
+                    'Conta',
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                  ),
+                ],
+              ),
+              Icon(
+                Icons.arrow_forward_ios,
+                size: 15,
+              )
+            ],
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Text(
+            eyeOpen == true ? 'R\$1.000,00' : '⚫⚫⚫⚫',
+            style: eyeOpen == true
+                ? TextStyle(fontWeight: FontWeight.bold, fontSize: 20)
+                : TextStyle(fontWeight: FontWeight.bold, fontSize: 10),
+          )
 
           //Accounts balance
         ],
